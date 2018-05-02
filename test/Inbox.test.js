75c05dc3af31d5f6a7862e74ff79ca22762dbf7f
Filipe Paiva <filipe.p@soltroiavillas.com>
2018-05-02 12:37:24 +0100

    tested mocha using simple Car class

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,34 @@
-Inbox.test.js
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //constructrer 
+
+const web3 = new Web3(ganache.provider()); // connecting to network
+
+// it - Rrun a testes and makes asserctions
+// describe - Groups together 'it functions'
+// beforeEach - iniciates something before testes
+
+// Learning how to use mocha with simple test
+class Car {
+	park() {
+		return 'stopped';
+	}
+
+	drive() {
+		return 'vroom';
+	}
+}
+
+//now lest use actual mocha and assert libraries
+
+describe('Car class methods', () => {
+	it('Can park, returning string stopped', () => {
+		const car = new Car;
+		assert.equal(car.park(), 'stopped');
+	});
+
+	it('Can drive, returning string vroom', () => {
+		const car = new Car;
+		assert.equal(car.drive(), 'vroom');
+	})
+});
