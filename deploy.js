ec376f5fc1a2cb6ae25b2c28c91b4979cee9071f
Filipe Paiva <filipe.p@soltroiavillas.com>
2018-05-03 22:47:51 +0100

    Still being reject.tested all the variables, and deploy() is being called. Contract still being rejected

--- deploy.js
+++ deploy.js
@@ -3,35 +3,28 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require('web3');
 const {interface, bytecode } = require('./compile');
 
-console.log(interface);
-console.log(bytecode);
-console.log('A........................');
-console.log(HDWalletProvider);
-
 //Wallet mnemonic + network
+// DO NOT PUT YOUR REAL ACCOUNT HERE
 const mnemonic = "miracle vanish dice sight dose reveal curve vault three fringe nurse buzz";
 const network = "https://rinkeby.infura.io/dRMvlwMXOYCMdB4fOJeP"
-console.log('1........................');
-console.log(mnemonic);
-console.log(network);
-console.log('2........................');
-const provider = new HDWalletProvider( mnemonic, network);
+const provider = new HDWalletProvider(mnemonic, network);
 
-console.log('3........................');
-console.log(provider);
-console.log('4........................');
+// console.log(provider);
 
 const web3 = new Web3(provider);
 
+// console.log(web3);
+
 const deploy = async () => {
-	console.log('inside deploy')
 	const accounts = await web3.eth.getAccounts();
 
 	console.log('attempting to deploy from account', accounts[0]);
 
 	const result = await new web3.eth.Contract(JSON.parse(interface))
 		.deploy ({ data: bytecode, arguments: ['hello!']})
-		.send ({ gas: '100000', from: accounts[0] });
+		.send ({ from: accounts[0], gas: 1000000});
 
 	console.log('contract deployed to', result.options.address);
 };
+
+deploy();
